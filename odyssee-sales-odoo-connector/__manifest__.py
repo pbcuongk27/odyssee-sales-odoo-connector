b980cd94adecc1692c0456db2b3851ec4cb523e0
Cuong Pham <32812341+pbcuongk27@users.noreply.github.com>
2022-09-26 11:41:45 +0700

    Create __manifest__.py

---
+++ odyssee-sales-odoo-connector/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Odyssee Sales Odoo Connector',
+    'version':'1.0',
+    'sequence': 1,
+    'description':'Odyssee Sales Odoo Connector',
+    'author':'Odyssee Sales',
+    'website':'https://odysseefieldservice.com/',
+    'license':'LGPL-3',
+    'installable':'False',
+    'images':['static/description/ods_description.gif'],
+    'live_test_url': 'https://portal.odysseemobile.com/marketplace?connect=odoosales',
+}
